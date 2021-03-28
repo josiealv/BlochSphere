a4c5693bdf024d3e1011fdb29c0f2d77fa09986c
Josie Jitzel Alvarez <josiealv@usc.edu>
2021-03-27 19:55:02 -0700

    trying to make gif pop up in new window with PySimpleGUI

---
+++ bloch.py
@@ -0,0 +1,54 @@
+import matplotlib as mpl
+from pylab import *
+from qutip import *
+from matplotlib import cm
+import imageio
+import PySimpleGUI as sg
+
+
+def animate_bloch(states, duration=0.1, save_all=False):
+    b = Bloch()
+    b.vector_color = ['r']
+    b.view = [-40, 30]
+    images = []
+    try:
+        length = len(states)
+    except:
+        length = 1
+        states = [states]
+    # normalize colors to the length of data ##
+    nrm = mpl.colors.Normalize(0, length)
+    colors = cm.cool(nrm(range(length)))  # options: cool, summer, winter, autumn etc.
+
+    # customize sphere properties ##
+    b.point_color = list(colors)  # options: 'r', 'g', 'b' etc.
+    b.point_marker = ['o']
+    b.point_size = [30]
+
+    for i in range(length):
+        b.clear()
+        b.add_states(states[i])
+        b.add_states(states[:(i + 1)], 'point')
+        if save_all:
+            b.save(dirc='tmp')  # saving images to tmp directory
+            filename = "tmp/bloch_%01d.png" % i
+        else:
+            filename = 'temp_file.png'
+            b.save(filename)
+        images.append(imageio.imread(filename))
+    imageio.mimsave('bloch_anim.gif', images, duration=duration)
+    layout = [
+        [sg.Image('bloch_anim.gif')],
+    ]
+    window = sg.Window('Bloch Sphere', layout)
+    event, values = window.read()
+    window.close()
+    sg.Popup(event, values[0])  
+
+
+states = []
+thetas = linspace(0, pi, 21)
+for theta in thetas:
+    states.append((cos(theta / 2) * basis(2, 0) + sin(theta / 2) * basis(2, 1)).unit())
+
+animate_bloch(states, duration=0.1, save_all=False)
