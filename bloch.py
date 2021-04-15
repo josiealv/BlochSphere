4d1d3c3ef8944a0ab4a21424c5e65db289acedf1
marisaclass <marisacl@usc.edu>
2021-04-14 20:56:24 -0400

    combined bloch.py and command.py

--- bloch.py
+++ bloch.py
@@ -1,14 +1,35 @@
+import argparse
 from cmath import pi, sin, cos
 
 import imageio
 from pylab import *
 from qutip import *
-from command_line import parse
+
+# to test: run "./command_line.py -i alpha -n time -n spacing
+def getOptions(args=sys.argv[1:]):
+    parser = argparse.ArgumentParser(description="Parses command.")
+    parser.add_argument("-i", "--input", help="Your input file.")
+    parser.add_argument("-o", "--output", help="Your destination output file.")
+    parser.add_argument("-n", "--num0", type=int, help="A number.")
+    parser.add_argument("-n", "--num1", type=int, help="A number.")
+    parser.add_argument("-v", "--verbose", dest='verbose', action='store_true', help="Verbose mode.")
+    opts = parser.parse_args(args)
+
+    return opts
+
+options = getOptions(sys.argv[1:])
+
+alpha = options.input
+beta = options.output
+spacing = options.num0
+time = options.num1
+
+items = [alpha, beta, spacing, time]
 
 def animate_bloch(states, duration=0.1, save_all=False):
     #call get_input from parse
     #extract input from list and then put into respective variables
-    for x in parse.get_input():
+    for x in items:
         alpha = x[0]
 
     b = Bloch3d()
