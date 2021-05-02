91acbcec969aa45224cc3ae099e796300143284b
Josie Jitzel Alvarez <josiealv@usc.edu>
2021-05-01 21:53:37 -0700

    trying to account for multiple state vectors that are changing

--- bloch.py
+++ bloch.py
@@ -36,23 +36,96 @@ def animate_bloch(states, duration=0.1, save_all=False):
         else:
             filename = 'temp_file.png'
             b.save(filename)
-        images.append(imageio.imread(filename))
-    imageio.mimsave('bloch_anim.gif', images, duration=duration)
-    displayGif()
+        images[j] = (imageio.imread(filename))  # combines all the images into one gif (one image)
 
-def displayGif():
-    #gif_info = Image.open("bloch_anim.gif")   , format="gif -index %i",
-    #frames = gif_info.n_frames
-    window = Tk()
-    window.title('Bloch Sphere')
-    canvas = Canvas(window, width = 500, height = 500)
-    canvas.pack()
-    bloch_gif = PhotoImage(file = 'bloch_anim.gif', master=window)
-    canvas.create_image(0, 0, image=bloch_gif, anchor = 'nw')
-    window.mainloop()
+    imageio.mimsave(save_file, images, duration=duration)
+    # calling gui.py file -> passing in the gif
+    gui.main(save_file)
 
-states = []
-thetas = linspace(0, pi, 21)
-for theta in thetas:
-    states.append((cos(theta / 2) * basis(2, 0) + sin(theta / 2) * basis(2, 1)).unit())
-animate_bloch(states, duration=0.1, save_all=False)
+def animate_multiple_states(vect_complex_arr, save_file, numV, duration=0.1, save_all=False):
+    azimuthal = -40  # can customize to chang view of gif
+    elevation = 20  # can customize to chang view of gif
+    bloch_obj = []
+    for i in range(0, numV):
+        bloch_obj[i] = Bloch()
+    
+
+    # customize sphere properties ##
+    b.point_color = list(colors)  # options: 'r', 'g', 'b' etc.
+    b.point_marker = ['o']
+    b.point_size = [30]
+
+def external_animate_bloch_multiple(numV, alpha_reals, alpha_imags, beta_reals, beta_imags, filename):
+    if(len(alpha_reals)!= numV):
+        print ("Number of state vectors does not equal amount inputted")
+        return 
+    if(len(alpha_reals)==len(beta_reals)):
+        for i in range (0, len(alpha_reals)):
+            if(len(alpha_reals[i])!=len(beta_reals[i])):
+                print("Alphas and Betas length do not match")
+                return
+        filename += ".gif"
+        vect_complex_arr = []      
+        for j in range (0, numV):
+            complex_arr = []
+            for k in range (0, len(alpha_reals[j])):
+                alpha = complex(alpha_reals[j][k], alpha_imags[j][k])
+                beta = complex(beta_reals[j][k], beta_imags[j][k])
+                # appending alpha and beta to complex_arr array
+                complex_arr.append((alpha * basis(2, 0) + beta * basis(2, 1)))
+            vect_complex_arr.append(complex_arr)
+        animate_multiple_states (filename, numV, vect_complex_arr)
+    else:
+        print("Alphas and Betas length do not match")
+
+def external_animate_bloch(alpha_reals, alpha_imags, beta_reals, beta_imags, filename):
+    if filename == "":
+        print("Need to have a filename")
+        return
+    # checking that parameters are > 0
+    if (len(alpha_reals) == len(beta_reals)) and len(alpha_imags) == len(beta_imags):
+        complex_arr = []    # creating array that we will fill with alpha and beta
+        for k in range(0, len(alpha_reals)):
+            # converting alpha nad beta inputs to complex #s
+            alpha = complex(alpha_reals[k], alpha_imags[k])
+            beta = complex(beta_reals[k], beta_imags[k])
+            # appending alpha and beta to complex_arr array
+            complex_arr.append((alpha * basis(2, 0) + beta * basis(2, 1)))
+        # adding file extension to the filename
+        filename += ".gif"
+        # calling function to create the bloch sphere gif
+        animate_bloch_states(complex_arr, filename, duration=0.1, save_all=False)
+    else:
+        print("Alphas and Betas length do not match")
+
+def main():
+    # if else statements are checking command line arguments are valid
+    if sys.argv[0] != "bloch.py":
+        pass
+    else:
+        num_args = len(sys.argv)
+        if num_args < 2:
+            print("Not enough command line arguments.")
+        elif num_args > 2:
+            print("Too many command line arguments.")
+        else:
+            #                                       argv[0]    argv[1]
+            # to run command looks like >> Python3 bloch.py [list of vectors]
+            # splitting each vector by the ","
+            arr_norm = sys.argv[1].split(",")
+            # making sure there are an alpha and a beta per vector inputs (so length of array needs to be divisible
+            # by 2)
+            if len(arr_norm) % 2 == 0:
+                complex_array = []
+                #   for each vector input:
+                #   1)  converting alpha/beta to complex and calculating the xy points for the bloch sphere
+                #   2)  appending to array
+                #   3)  calling function to create the bloch sphere gif
+                for i in range(0, len(arr_norm), 2):
+                    complex_array.append((complex(arr_norm[i]) * basis(2, 0) + complex(arr_norm[i + 1]) * basis(2, 1)))
+                filename = input("type in filename (without extension) e.g. 'test_file' ") + ".gif"
+                animate_bloch_states(complex_array, filename, duration=0.1, save_all=False)
+            else:
+                print("Each state vector must have an alpha and a beta")
+
+main()
---
+++ test_external_animate.py
@@ -0,0 +1,34 @@
+from bloch import external_animate_bloch
+import numpy as np
+
+length = 100
+betasR = [None] * length
+alphasR = [None] * length
+alphasI = [None] * length
+betasI = [None] * length
+omega = .05  # customize omega here
+gamma = .01  # customize gamma here
+
+# ---------------
+#   For the file name, provide the absolute path and then the name of what you want it to be saved without an extension
+#   Ex: /users/name/Desktop/{name_you_want}
+#   Reminder: DO NOT ADD AN EXTENSION TO THE NAME (ie. {...}.gif)
+#   If no relative path is provided, it will save to the directory in which the program is running in.
+# ---------------
+file_no_extension = "test"
+phi = (np.pi * 3) / 4
+
+# example 1 (exponential decay)
+for i in range(0, 100):
+    alphasR[i] = (np.cos(omega * i) * np.exp(-i * gamma))
+    alphasI[i] = 0
+    betasR[i] = (np.sin(omega * i) * np.exp(-i * gamma) * np.cos(phi))
+    betasI[i] = (np.sin(omega * i) * np.exp(-i * gamma) * np.sin(phi))
+
+# # # example 2 (Rabi oscillations)
+# for i in range(0, 100):
+#     alphasR[i] = (np.cos(omega * i))
+#     alphasI[i] = 0
+#     betasR[i] = (np.sin(omega * i))
+#     betasI[i] = 0
+# b.external_animate_bloch(alphasR, alphasI, betasR, betasI, 'test2')
